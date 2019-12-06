90a03eb4b27052ed0cb4d64647ca41f4b67488d2
Rachel Gould <rachel@rachelgould.dev>
2019-12-05 18:08:03 -0800

    get list of order ids from csv

--- order-country.py
+++ order-country.py
@@ -1,11 +1,13 @@
 # Install:
 # pip install woocommerce
 # Put your API credentials in secretsEXAMPLE.py, and rename it to secrets.py
+# Upload a .csv containing ONLY the order IDs you want to edit (see orders.csv as an example)
 
 # Setup:
 from woocommerce import API
 from secrets import set_variables
 import os
+import csv
 
 def check_environment():
   return all (k in os.environ for k in('STORE_URL', 'CONSUMER_KEY', 'CONSUMER_SECRET'))
@@ -19,6 +21,17 @@ def make_api():
       version="wc/v3"
   )
 
+def get_order_ids():
+  ids = list()
+  with open('orders.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+      line_count = line_count + 1
+      ids.append(row[0])
+    print(str(line_count) + " orders in list.")
+  return ids
+
 set_variables()
 if check_environment():
   print("Authentication details are set.")
@@ -26,5 +39,8 @@ else:
   print("You must set up your authentication details.")
 
 wcapi = make_api()
+order_ids = get_order_ids()
 
-print(wcapi.get("orders/727").json())
+for id in order_ids:
+  print(id)
+  print(wcapi.get("orders/{id}").json())
