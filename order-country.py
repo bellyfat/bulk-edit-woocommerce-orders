b2528e7e95b57c45d519dc5f8029968eeabe49ed
Rachel Gould <rachel@rachelgould.dev>
2019-12-05 17:56:52 -0800

    wcapi variable for woo api is returning data

--- order-country.py
+++ order-country.py
@@ -10,5 +10,21 @@ import os
 def check_environment():
   return all (k in os.environ for k in('STORE_URL', 'CONSUMER_KEY', 'CONSUMER_SECRET'))
 
+def make_api():
+  return API(
+      url= os.environ['STORE_URL'],
+      consumer_key= os.environ['CONSUMER_KEY'],
+      consumer_secret= os.environ['CONSUMER_SECRET'],
+      wp_api=True,
+      version="wc/v3"
+  )
+
 set_variables()
-print(check_environment())
+if check_environment():
+  print("Authentication details are set.")
+else:
+  print("You must set up your authentication details.")
+
+wcapi = make_api()
+
+print(wcapi.get("orders/727").json())
